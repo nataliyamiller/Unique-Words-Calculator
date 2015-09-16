2ea0720ab3164908acc67861e4e52bbd89d562d3
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-16 15:16:26 -0700

    Passes specs for identifying if array has unique words

--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,8 @@ describe('uniqueWordIdentifier', function() {
   it("returns true if array has duplicate words", function() {
     expect(uniqueWordIdentifier("hello world hello")).to.equal(true);
   });
+
+  it("returns false if array doesn't have duplicate words", function() {
+    expect(uniqueWordIdentifier("Great world hello")).to.equal(false);
+  });
 });
