9184013d6141eaa02dea56cb30dc163fd2d132ec
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-16 15:39:37 -0700

    Passes spec for calculating unique words in user's input

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,17 @@ var uniqueWordIdentifier = function(userInput) {
   return false;
 };
 
+var uniqueWordCalculator = function(userInput) {
+  var words = userInput.split(" ");
+  var results = {};
 
+  for (var i = 0; i < words.length; i++) {
+    if (!results[words[i]]) {
+      results[(words[i])] = 1;
+    } else {
+      results[words[i]] +=1;
+    }
 
-
-// var uniqueWordCalculator = function(userInput) {
-//   var arrayInput = (sentenceSplitter(userInput));
-//   result = {};
-//
-//   }
-// }
+    }
+    return results;
+  }
--- spec/specs.js
+++ spec/specs.js
@@ -8,3 +8,10 @@ describe('uniqueWordIdentifier', function() {
     expect(uniqueWordIdentifier("Great world hello")).to.equal(false);
   });
 });
+
+
+describe('uniquWordCalculator', function() {
+  it("returns an object that contains unique words and amounts those words appear in the user's input", function() {
+    expect(uniqueWordCalculator("hello world hello hello again")).to.eql({"hello": 3, "world": 1, "again": 1});
+  });
+});
