157ba8ed1d0fec56a9067cc3fa7be19f0b94c6a9
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-16 22:18:17 -0700

    Updated some wording for better clarity

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var updatedUserInput = function(userInput) {
   return userInput.toLowerCase().replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ");
-}
+};
 
 var wordsObject = function(updatedInput) {
   var results = {};
@@ -12,7 +12,7 @@ var wordsObject = function(updatedInput) {
     }
   }
     return results;
-}
+};
 
 var sortedValues = function(wordsObject) {
   var sorted = [];
@@ -21,7 +21,7 @@ var sortedValues = function(wordsObject) {
     sorted.sort(function(a, b) {return b[1] - a[1]});
   }
     return sorted;
-}
+};
 
 var isUserUnique = function(userInput) {
   var sortedValues = uniqueWordsCalculator(userInput);
@@ -30,10 +30,10 @@ var isUserUnique = function(userInput) {
   } else {
     return false;
   }
-}
+};
 
 var uniqueWordsCalculator = function(userInput) {
   var updatedInput = updatedUserInput(userInput);
   var inputAsObject = wordsObject(updatedInput);
   return sortedValues(inputAsObject);
-}
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,4 @@
 
-
 describe('updatedUserInput', function() {
   it("removes special characters and returns user's input as an array of words in lower case format", function() {
     expect(updatedUserInput("Hello world, hello: I am here")).to.eql(["hello", "world", "hello", "i", "am", "here"]);
@@ -7,20 +6,20 @@ describe('updatedUserInput', function() {
 });
 
 describe('wordsObject', function() {
-  it("turnes array of user's words into an object of unique words as keys, and numbers of those words duplications as values", function() {
+  it("turns an array of user's words into an object of unique words as keys and numbers of duplicates as values", function() {
     expect(wordsObject(["hello", "world", "hello", "hello", "again"])).to.eql({"hello": 3, "world": 1, "again": 1});
   });
 });
 
 describe('sortedValues', function() {
-  it("will turn an object to an array of arrays and sort it by numeric values in a descending order", function() {
+  it("turns an object into an array of arrays and sort it by numeric values in a descending order", function() {
     var userObject = {"hello": 3, "world": 1};
     expect(sortedValues(userObject)).to.eql([["hello", 3], ["world", 1]]);
   });
 });
 
 describe('isUserUnique', function() {
-  it("returns true if user's input doesn't have duplicate words", function() {
+  it("returns true if user's input doesn't have any duplicate words", function() {
     expect(isUserUnique("Hello, great world")).to.equal(true);
   });
 
@@ -30,7 +29,7 @@ describe('isUserUnique', function() {
 });
 
 describe('uniqueWordsCalculator', function() {
-  it("will accept users input and return a sorted array of arrays", function() {
+  it("accepts user's input and turns it into a sorted array of arrays", function() {
     expect(uniqueWordsCalculator("Hello world, hello: again hello world")).to.eql([["hello", 3], ["world", 2], ["again", 1]]);
   });
 });
